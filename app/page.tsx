378c46eadd6aa3c713440862574a8c90e7113125
Rishabh <dplayarrr@gmail.com>
2024-09-21 03:49:26 +0530

    Added avatar in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">nice</h1>
+    <div className="flex justify-between">
+      <div>
+        <p className="text-sm tracking-tight text-muted-foreground">
+          Good Morning!
+        </p>
+        <h4 className="text-lg font-bold">Rishabh</h4>
+      </div>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </div>
   );
 }
