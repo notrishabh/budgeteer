beecc207ee9a4226deb6e88e1f1db49962118fc2
Rishabh <dplayarrr@gmail.com>
2024-10-04 00:49:16 +0530

    Used react-hook-form in login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,21 @@
 
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Login() {
-  // use react hook form here with shadcn
   const router = useRouter();
   const { mutate } = useMutation({
     mutationFn: async (body: { username: string; password: string }) => {
@@ -19,25 +31,57 @@ export default function Login() {
       router.push("/");
     },
   });
-  const handleSumbit = () => {
-    const x = {
-      username: "tester",
-      password: "hi",
-    };
-    mutate(x);
-  };
+
+  const formSchema = z.object({
+    username: z.string(),
+    password: z.string(),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    mutate(data);
+  }
+
   return (
     <section>
-      <form
-        className="flex flex-col gap-4"
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button onClick={handleSumbit} className="p-4 bg-red-500">
-          Login
-        </button>
-      </form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full p-6 rounded-lg">
+            Sign In
+          </Button>
+        </form>
+      </Form>
     </section>
   );
 }
